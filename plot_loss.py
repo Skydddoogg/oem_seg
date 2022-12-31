3b9f7e258648c3f4701a00d0f699c606251601fc
Thanawat Lodkaew <59070071@it.kmitl.ac.th>
2022-12-31 16:50:37 +0900

    added plot function

---
+++ plot_loss.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import argparse
+import ast
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model')
+
+args = parser.parse_args()
+
+d = {'x': [], 'y': [], 'label': []}
+with open(os.path.join('outputs', f'{args.model}_train_loss.txt')) as f:
+    loss_list = ast.literal_eval(f.readlines()[0])
+    d['x'] += [i for i in range(len(loss_list))]
+    d['y'] += loss_list
+    d['label'] += ['train'] * len(loss_list)
+
+with open(os.path.join('outputs', f'{args.model}_val_loss.txt')) as f:
+    loss_list = ast.literal_eval(f.readlines()[0])
+    d['x'] += [i for i in range(len(loss_list))]
+    d['y'] += loss_list
+    d['label'] += ['val'] * len(loss_list)
+
+d['y'] = np.array(d['y'])
+d['y'] = (d['y'] - min(d['y'])) / (max(d['y']) - min(d['y']))
+
+df = pd.DataFrame(data=d)
+
+sns.lineplot(
+        data=df, 
+        x="x", y="y", 
+        hue='label', 
+        style="label"
+)
+
+plt.title(args.model) # Edit here
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.grid()
+plt.legend()
+plt.tight_layout()
+plt.savefig(os.path.join('outputs', f'{args.model}.png'), dpi=300) # Edit here
