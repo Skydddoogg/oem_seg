84f3216f21c06f1bc70a29effcc6a3a7db74e317
skydddoogg <lodkaew-thanawat@g.ecc.u-tokyo.ac.jp>
2023-01-01 13:50:20 +0900

    added arguments to testing func

--- demo_testing.py
+++ demo_testing.py
@@ -7,17 +7,28 @@ import torch
 import cv2
 import oem
 from pathlib import Path
+import segmentation_models_pytorch as smp
+import argparse
+from tqdm import tqdm
 
 warnings.filterwarnings("ignore")
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model')
+    args = parser.parse_args()
+
     start = time.time()
 
+    BACKBONE = 'resnet18'
+    
     OEM_DATA_DIR = "OpenEarthMap_Mini"
     TEST_LIST = os.path.join(OEM_DATA_DIR, "test.txt")
 
     N_CLASSES = 9
-    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+    # DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+    DEVICE = 'cpu'
     PREDS_DIR = "predictions"
     os.makedirs(PREDS_DIR, exist_ok=True)
 
@@ -34,22 +45,27 @@ if __name__ == "__main__":
         testing=True,
     )
 
-    network = oem.networks.UNet(in_channels=3, n_classes=N_CLASSES)
+    network = smp.UnetPlusPlus(
+        encoder_name=BACKBONE,
+        in_channels=3,
+        classes=N_CLASSES,   
+    )
+
     network = oem.utils.load_checkpoint(
         network,
-        model_name="model2.pth",
+        model_name=f"{args.model}.pth",
         model_dir="outputs",
     )
 
     network.eval().to(DEVICE)
-    for idx in range(len(test_fns)):
+    for idx in tqdm(range(len(test_fns))):
         img, fn = test_data[idx][0], test_data[idx][2]
 
         with torch.no_grad():
             prd = network(img.unsqueeze(0).to(DEVICE)).squeeze(0).cpu()
         prd = oem.utils.make_rgb(np.argmax(prd.numpy(), axis=0))
+        fout = os.path.join(PREDS_DIR, fn.split("\\")[-1].split('.')[0] + '.png')
 
-        fout = os.path.join(PREDS_DIR, fn.split("\\")[-1])
         with rasterio.open(fn, "r") as src:
             profile = src.profile
             prd = cv2.resize(
@@ -57,6 +73,5 @@ if __name__ == "__main__":
                 (profile["width"], profile["height"]),
                 interpolation=cv2.INTER_NEAREST,
             )
-            with rasterio.open(fout, "w", **profile) as dst:
-                for idx in src.indexes:
-                    dst.write(prd[:, :, idx - 1], idx)
+
+            cv2.imwrite(fout, cv2.cvtColor(prd, cv2.COLOR_RGB2BGR))
