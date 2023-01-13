262e07d3345b7a7dbc05d1a66bd4665da8de17e8
Thanawat Lodkaew <59070071@it.kmitl.ac.th>
2023-01-13 15:14:59 +0900

    removed unused files

--- oem/losses.py
+++ oem/losses.py
@@ -20,37 +20,6 @@ class JaccardLoss(nn.Module):
             losses += 1 - metrics.fscore(ypr, ygt)
         return losses
 
-class GroupedJaccardLoss(nn.Module):
-    def __init__(self, class_group, class_weights=1.0):
-        super().__init__()
-        self.class_weights = torch.tensor(class_weights)
-        self.name = "GroupedJaccard"
-        self.class_group = class_group
-
-    def forward(self, input, target):
-        input = torch.softmax(input, dim=1)
-
-        total_loss = 0
-
-        # calculate average loss of majority samples
-        losses = 0
-        for i in self.class_group['majority']:
-            ypr = input[:, i, :, :]
-            ygt = target[:, i, :, :]
-            losses += 1 - metrics.fscore(ypr, ygt)
-        total_loss += losses
-
-        # calculate average loss of minority samples
-        losses = 0
-        for i in self.class_group['minority'][1:]:  # background is not included
-            ypr = input[:, i, :, :]
-            ygt = target[:, i, :, :]
-            losses += 1 - metrics.fscore(ypr, ygt)
-        total_loss += losses
-
-        return total_loss
-
-
 # ----------------
 # --- DiceLoss ---
 # ----------------
@@ -68,37 +37,6 @@ class DiceLoss(nn.Module):
             ygt = target[:, i, :, :]
             losses += 1 - metrics.iou(ypr, ygt)
         return losses
-
-class GroupedDiceLoss(nn.Module):
-    def __init__(self, class_group, class_weights=1.0):
-        super().__init__()
-        self.class_weights = torch.tensor(class_weights)
-        self.name = "GroupedDice"
-        self.class_group = class_group
-
-    def forward(self, input, target):
-        input = torch.softmax(input, dim=1)
-
-        total_loss = 0
-
-        # calculate average loss of majority samples
-        losses = 0
-        for i in self.class_group['majority']:
-            ypr = input[:, i, :, :]
-            ygt = target[:, i, :, :]
-            losses += 1 - metrics.iou(ypr, ygt)
-        total_loss += losses
-
-        # calculate average loss of minority samples
-        losses = 0
-        for i in self.class_group['minority']:
-            ypr = input[:, i, :, :]
-            ygt = target[:, i, :, :]
-            losses += 1 - metrics.iou(ypr, ygt)
-        total_loss += losses
-
-        return total_loss
-
 
 # ------------------------
 # --- CEWithLogitsLoss ---
@@ -195,10 +133,10 @@ class GroupedFocalLoss(nn.Module):
             pt = torch.exp(-bce_loss)
             focal_loss = self.alpha * (1 - pt) ** self.gamma * bce_loss
             losses += focal_loss.mean()
-        total_loss += losses
+        total_loss += losses / len(self.class_group['majority'])
 
         losses = 0
-        for i in self.class_group['minority']:  # background is not included
+        for i in self.class_group['minority']:
 
             ypr = input[:, i, :, :]
             ygt = target[:, i, :, :]
@@ -210,7 +148,7 @@ class GroupedFocalLoss(nn.Module):
             pt = torch.exp(-bce_loss)
             focal_loss = self.alpha * (1 - pt) ** self.gamma * bce_loss
             losses += focal_loss.mean()
-        total_loss += losses
+        total_loss += losses / len(self.class_group['minority'])
 
         return total_loss
 
@@ -254,135 +192,6 @@ class MCCLoss(nn.Module):
 
         return loss
 
-class HybridMCCLoss(nn.Module):
-    """
-    Compute Matthews Correlation Coefficient Loss for image segmentation
-    Reference: https://github.com/kakumarabhishek/MCC-Loss
-    """
-
-    def __init__(self, eps: float = 1e-5):
-        super().__init__()
-        self.eps = eps
-        self.name = "HybridMCC"
-
-    def forward(self, input, target):
-        losses = 0
-        for i in range(input.shape[1]):
-
-            ypr = input[:, i, :, :]
-            ygt = target[:, i, :, :]
-            
-            bs = ygt.shape[0]
-
-            ypr = torch.sigmoid(ypr)
-
-            ygt = ygt.view(bs, 1, -1)
-            ypr = ypr.view(bs, 1, -1)
-
-            tp = torch.sum(torch.mul(ypr, ygt)) + self.eps
-            tn = torch.sum(torch.mul((1 - ypr), (1 - ygt))) + self.eps
-            fp = torch.sum(torch.mul(ypr, (1 - ygt))) + self.eps
-            fn = torch.sum(torch.mul((1 - ypr), ygt)) + self.eps
-
-            numerator = torch.mul(tp, tn) - torch.mul(fp, fn)
-            denominator = torch.sqrt(
-                torch.add(tp, fp)
-                * torch.add(tp, fn)
-                * torch.add(tn, fp)
-                * torch.add(tn, fn)
-            )
-
-            mcc = torch.div(numerator.sum(), denominator.sum())
-            loss = 1.0 - mcc
-
-            losses += loss
-
-        return losses
-
-class GroupedMCCLoss(nn.Module):
-    """
-    Compute Matthews Correlation Coefficient Loss for image segmentation
-    Reference: https://github.com/kakumarabhishek/MCC-Loss
-    """
-
-    def __init__(self, class_group: dict, eps: float = 1e-5):
-        super().__init__()
-        self.eps = eps
-        self.name = "GroupedMCC"
-        self.class_group = class_group
-
-    def forward(self, input, target):
-
-        total_loss = 0
-
-        losses = 0
-        for i in self.class_group['majority']:
-
-            ypr = input[:, i, :, :]
-            ygt = target[:, i, :, :]
-            
-            bs = ygt.shape[0]
-
-            ypr = torch.sigmoid(ypr)
-
-            ygt = ygt.view(bs, 1, -1)
-            ypr = ypr.view(bs, 1, -1)
-
-            tp = torch.sum(torch.mul(ypr, ygt)) + self.eps
-            tn = torch.sum(torch.mul((1 - ypr), (1 - ygt))) + self.eps
-            fp = torch.sum(torch.mul(ypr, (1 - ygt))) + self.eps
-            fn = torch.sum(torch.mul((1 - ypr), ygt)) + self.eps
-
-            numerator = torch.mul(tp, tn) - torch.mul(fp, fn)
-            denominator = torch.sqrt(
-                torch.add(tp, fp)
-                * torch.add(tp, fn)
-                * torch.add(tn, fp)
-                * torch.add(tn, fn)
-            )
-
-            mcc = torch.div(numerator.sum(), denominator.sum())
-            loss = 1.0 - mcc
-
-            losses += loss
-
-        total_loss += losses
-
-        losses = 0
-        for i in self.class_group['minority']:
-
-            ypr = input[:, i, :, :]
-            ygt = target[:, i, :, :]
-            
-            bs = ygt.shape[0]
-
-            ypr = torch.sigmoid(ypr)
-
-            ygt = ygt.view(bs, 1, -1)
-            ypr = ypr.view(bs, 1, -1)
-
-            tp = torch.sum(torch.mul(ypr, ygt)) + self.eps
-            tn = torch.sum(torch.mul((1 - ypr), (1 - ygt))) + self.eps
-            fp = torch.sum(torch.mul(ypr, (1 - ygt))) + self.eps
-            fn = torch.sum(torch.mul((1 - ypr), ygt)) + self.eps
-
-            numerator = torch.mul(tp, tn) - torch.mul(fp, fn)
-            denominator = torch.sqrt(
-                torch.add(tp, fp)
-                * torch.add(tp, fn)
-                * torch.add(tn, fp)
-                * torch.add(tn, fn)
-            )
-
-            mcc = torch.div(numerator.sum(), denominator.sum())
-            loss = 1.0 - mcc
-
-            losses += loss
-
-        total_loss += losses
-
-        return total_loss
-
 # ----------------
 # --- OHEMLoss ---
 # ----------------
@@ -420,124 +229,3 @@ class OHEMBCELoss(nn.Module):
             )[: self.min_kept]
             kept_flag.contiguous().view(-1)[hardest_examples] = True
         return self.criterion(input[kept_flag, 0], target[kept_flag, 0].float())
-
-class OHEMFLLoss(nn.Module):
-    """
-    Taken and modified from:
-    https://github.com/PkuRainBow/OCNet.pytorch/blob/master/utils/loss.py
-    """
-
-    def __init__(self, thresh=0.7, min_kept=10000):
-        super(OHEMFLLoss, self).__init__()
-        self.thresh = float(thresh)
-        self.min_kept = int(min_kept)
-        self.criterion = FocalLoss(gamma=2, alpha=0.25)
-        self.name = "OHEMFL"
-
-    def forward(self, input, target):
-
-        probs = torch.sigmoid(input)[:, 0, :, :].float()
-        ygt = target[:, 0, :, :].float()
-
-        # keep hard examples
-        kept_flag = torch.zeros_like(probs).bool()
-        # foreground pixels with low foreground probability
-        kept_flag[ygt == 1] = probs[ygt == 1] <= self.thresh
-        # background pixel with high foreground probability
-        kept_flag[ygt == 0] = probs[ygt == 0] >= 1 - self.thresh
-
-        if kept_flag.sum() < self.min_kept:
-            # hardest examples have a probability closest to 0.5.
-            # The network is very unsure whether they belong to the foreground
-            # prob=1 or background prob=0
-            hardest_examples = torch.argsort(
-                torch.abs(probs - 0.5).contiguous().view(-1)
-            )[: self.min_kept]
-            kept_flag.contiguous().view(-1)[hardest_examples] = True
-        return self.criterion(input[kept_flag, 0], target[kept_flag, 0].float())
-
-
-class HybridOHEMBCELoss(nn.Module):
-    """
-    Taken and modified from:
-    https://github.com/PkuRainBow/OCNet.pytorch/blob/master/utils/loss.py
-    """
-
-    def __init__(self, thresh=0.7, min_kept=10000):
-        super(HybridOHEMBCELoss, self).__init__()
-        self.thresh = float(thresh)
-        self.min_kept = int(min_kept)
-        self.criterion = torch.nn.BCEWithLogitsLoss()
-        self.name = "HybridOHEM"
-
-    def forward(self, input, target):
-        losses = 0
-        for i in range(1, input.shape[1]):
-
-            class_wise_input = input[:, [0, i], :, :]
-            class_wise_target = target[:, [0, i], :, :]
-
-            probs = torch.sigmoid(class_wise_input)[:, 0, :, :].float()
-            ygt = class_wise_target[:, 0, :, :].float()
-
-            # keep hard examples
-            kept_flag = torch.zeros_like(probs).bool()
-            # foreground pixels with low foreground probability
-            kept_flag[ygt == 1] = probs[ygt == 1] <= self.thresh
-            # background pixel with high foreground probability
-            kept_flag[ygt == 0] = probs[ygt == 0] >= 1 - self.thresh
-
-            if kept_flag.sum() < self.min_kept:
-                # hardest examples have a probability closest to 0.5.
-                # The network is very unsure whether they belong to the foreground
-                # prob=1 or background prob=0
-                hardest_examples = torch.argsort(
-                    torch.abs(probs - 0.5).contiguous().view(-1)
-                )[: self.min_kept]
-                kept_flag.contiguous().view(-1)[hardest_examples] = True
-            losses += self.criterion(class_wise_input[kept_flag, 0], class_wise_target[kept_flag, 0].float())
-        
-        return losses
-
-class HybridOHEMFLLoss(nn.Module):
-    """
-    Taken and modified from:
-    https://github.com/PkuRainBow/OCNet.pytorch/blob/master/utils/loss.py
-    """
-
-    def __init__(self, thresh=0.7, min_kept=10000):
-        super(HybridOHEMFLLoss, self).__init__()
-        self.thresh = float(thresh)
-        self.min_kept = int(min_kept)
-        self.criterion = FocalLoss(gamma=2, alpha=0.25)
-        self.name = "HybridOHEMFL"
-
-    def forward(self, input, target):
-        losses = 0
-        for i in range(1, input.shape[1]):
-
-            class_wise_input = input[:, [0, i], :, :]
-            class_wise_target = target[:, [0, i], :, :]
-
-            probs = torch.sigmoid(class_wise_input)[:, 0, :, :].float()
-            ygt = class_wise_target[:, 0, :, :].float()
-
-            # keep hard examples
-            kept_flag = torch.zeros_like(probs).bool()
-            # foreground pixels with low foreground probability
-            kept_flag[ygt == 1] = probs[ygt == 1] <= self.thresh
-            # background pixel with high foreground probability
-            kept_flag[ygt == 0] = probs[ygt == 0] >= 1 - self.thresh
-
-            if kept_flag.sum() < self.min_kept:
-                # hardest examples have a probability closest to 0.5.
-                # The network is very unsure whether they belong to the foreground
-                # prob=1 or background prob=0
-                hardest_examples = torch.argsort(
-                    torch.abs(probs - 0.5).contiguous().view(-1)
-                )[: self.min_kept]
-                kept_flag.contiguous().view(-1)[hardest_examples] = True
-            losses += self.criterion(class_wise_input[kept_flag, 0], class_wise_target[kept_flag, 0].float())
-        
-        return losses
-
